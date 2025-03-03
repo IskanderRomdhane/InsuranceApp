1e1ffe7b910f44ef099d4fe193cda439296077e7
Iskander Romdhane <skander20034@gmail.com>
2025-03-03 11:42:01 +0000

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
 import Root from "./Pages/Root/Root.jsx"
-import {ReactKeycloakProvider} from "@react-keycloak/web";
-import keycloak from './config/keycloak';
 import Login from './Pages/Login.jsx'
 import Unauthorized from './Pages/Unauthorized.jsx'
 import AboutUs from "./Pages/AboutUs.jsx";
+import ContactUs from "./Pages/ContactUs/ContactUs.jsx"
+import Dashboard from './Pages/User/Dashboard.jsx'
 function App() {
 
   return (
-      <ReactKeycloakProvider authClient={keycloak} initOptions={{ onLoad: 'check-sso' }}>
       <Router>
           <Routes>
               <Route path="/" element={<Root />} />
               <Route path='/login' element={<Login />} />
               <Route path='/unauthorized' element={<Unauthorized />} />
               <Route path='/aboutus' element={<AboutUs />} />
+              <Route path='/contactus' element={<ContactUs />} />
+              <Route path='/dashboard' element={<Dashboard />} />
           </Routes>
       </Router>
-          </ReactKeycloakProvider>
   )
 }
 
