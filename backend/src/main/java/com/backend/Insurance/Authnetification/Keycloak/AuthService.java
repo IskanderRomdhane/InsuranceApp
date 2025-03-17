a4ae5cd75cb9644bd25094b2532dabd09f4118e9
Iskander Romdhane <skander20034@gmail.com>
2025-03-17 11:25:11 +0000

    Added Keycloak User Registration

--- backend/src/main/java/com/backend/Insurance/Authnetification/Keycloak/AuthService.java
+++ backend/src/main/java/com/backend/Insurance/Authnetification/Keycloak/AuthService.java
@@ -1,19 +1,22 @@
 package com.backend.Insurance.Authnetification.Keycloak;
 
+import com.backend.Insurance.Authnetification.Config.KeycloakConfig;
 import com.backend.Insurance.Authnetification.DTOs.LoginRequest;
 import com.backend.Insurance.Authnetification.Security.JwtParser;
-import com.nimbusds.jose.shaded.gson.JsonObject;
+import com.backend.Insurance.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 import java.util.Map;
 
 
@@ -31,7 +34,7 @@ public class AuthService {
     private String clientId;
 
     private final JwtParser jwtParser;
-
+    private final UserRepository userRepository;
     public ResponseEntity<String> login(LoginRequest loginDto) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8080/realms/InsuranceApp/protocol/openid-connect/token";
@@ -40,7 +43,7 @@ public class AuthService {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("grant_type", "password");
+        requestBody.add("grant_type", "refresh_token");
         requestBody.add("client_id", clientId);
         requestBody.add("username", loginDto.getUsername());
         requestBody.add("password", loginDto.getPassword());
@@ -76,7 +79,6 @@ public class AuthService {
             requestBody.add("grant_type", "refresh_token");
             requestBody.add("client_id", "Insurance");
             requestBody.add("refresh_token", refreshToken.get("refreshToken"));
-            System.out.println("refresh token: " + refreshToken.get("refreshToken"));
             HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(requestBody, headers);
 
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
@@ -86,8 +88,29 @@ public class AuthService {
         }
     }
 
+    public Integer register(UserRepresentation userRep) {
+        try {
+            UserRepresentation user = new UserRepresentation();
+            user.setUsername(userRep.getUsername());
+            user.setFirstName(userRep.getFirstName());
+            user.setLastName(userRep.getLastName());
+            user.setEmail(userRep.getEmail());
+            user.setCredentials(userRep.getCredentials());
+            user.setEnabled(true);
+            user.setEmailVerified(true);
 
+            UsersResource instance = KeycloakConfig.getInstance().realm(realm).users();
+            Response response = instance.create(user);
 
+            if (response.getStatus() == 201) {
+                return response.getStatus();
+            } else {
+                throw new RuntimeException("Failed to create user: HTTP Status : " + response.getStatus());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected error while registering user", e);
+        }
+    }
 
 
 }
