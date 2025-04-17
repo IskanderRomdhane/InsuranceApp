9fe57a5397d7bbaa76f0d3c0b5f23c10e12c33c2
SadekBlk <sadek.belkhiria@polytechnicien.tn>
2025-04-17 17:47:19 +0200

    notification

--- backend/src/main/java/com/backend/Insurance/notification/NotificationRepository.java
+++ backend/src/main/java/com/backend/Insurance/notification/NotificationRepository.java
@@ -1,11 +1,4 @@
 package com.backend.Insurance.notification;
 
-import com.backend.Insurance.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUser(User user);
-    List<Notification> findByUserId(Long userId);
+public interface NotificationRepository {
 }
