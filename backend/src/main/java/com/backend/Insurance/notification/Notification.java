a5b01ad7113de4e25b4a8248c745b60ae8f15ec6
SadekBlk <sadek.belkhiria@polytechnicien.tn>
2025-04-17 17:47:54 +0200

    notification

--- backend/src/main/java/com/backend/Insurance/notification/Notification.java
+++ backend/src/main/java/com/backend/Insurance/notification/Notification.java
@@ -1,5 +1,9 @@
 package com.backend.Insurance.notification;
 
+import com.backend.Insurance.User.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -8,8 +12,24 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @ToString
+@Entity
 public class Notification {
 
+    //    private NotificationStatus status;
+//    private String message;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //    @Enumerated(EnumType.STRING)
     private NotificationStatus status;
+
     private String message;
+
+    //     optionally you can link the notification to a user (example)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    @JsonIgnore
+    private User user;
 }
