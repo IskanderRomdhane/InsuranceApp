73017e6f8d72c830e8dc4f73500768224fe4f4bd
Iskander Romdhane <skander20034@gmail.com>
2025-04-24 14:53:11 +0100

    Added Habilitation class inherit sinistre class

---
+++ backend/src/main/java/com/backend/Insurance/Sinistre/Habilitation/Habilitation.java
@@ -0,0 +1,19 @@
+package com.backend.Insurance.Sinistre.Habilitation;
+
+import com.backend.Insurance.Sinistre.Sinistre;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@DiscriminatorValue("Habilitation")
+public class Habilitation extends Sinistre {
+    private String propertyAddress;
+    private String damageType;
+}
---
+++ backend/src/main/java/com/backend/Insurance/Sinistre/Habilitation/Service/HabilitationService.java
@@ -0,0 +1,118 @@
+package com.backend.Insurance.Sinistre.Habilitation.Service;
+
+import com.backend.Insurance.Document.Document;
+import com.backend.Insurance.Document.DocumentRepository;
+import com.backend.Insurance.Document.DocumentService;
+import com.backend.Insurance.Emails.EmailSenderService;
+import com.backend.Insurance.Image.Image;
+import com.backend.Insurance.Image.ImageRepository;
+import com.backend.Insurance.Image.ImageService;
+import com.backend.Insurance.Sinistre.Enums.Etat;
+import com.backend.Insurance.Sinistre.Habilitation.DTOS.HabilitationDTO;
+import com.backend.Insurance.Sinistre.Habilitation.Habilitation;
+import com.backend.Insurance.Sinistre.Habilitation.HabilitationRepository;
+import com.backend.Insurance.Sinistre.Sinistre;
+import com.backend.Insurance.Sinistre.SinistreRepository;
+import com.backend.Insurance.User.User;
+import com.backend.Insurance.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class HabilitationService {
+    private final UserRepository userRepository;
+    private final EmailSenderService emailSenderService;
+    private final ImageService imageService;
+    private final ImageRepository imageRepository;
+    private final SinistreRepository sinistreRepository;
+    private final DocumentService documentService;
+    private final DocumentRepository documentRepository;
+    private final HabilitationRepository habilitationRepository;
+    public ResponseEntity<String> CreateAutoMobileSinistre(MultipartFile document, MultipartFile image, HabilitationDTO habilitationDTO) {
+        Optional<User> optionalUser = userRepository.findById(habilitationDTO.getUserId());
+        if (optionalUser.isPresent()) {
+            User foundUser = optionalUser.get();
+            List<Image> imageList = new ArrayList<>();
+            List<Document> documentList = new ArrayList<>();
+            Habilitation sinistre = Habilitation.builder()
+                    .images(imageList)
+                    .document(documentList)
+                    .description(habilitationDTO.getDescriptionSinistre())
+                    .object(habilitationDTO.getObjectSinistre())
+                    .etat(Etat.PENDING)
+                    .user(foundUser)
+                    .date(LocalDateTime.now())
+                    .amount(habilitationDTO.getAmount())
+                    .propertyAddress(habilitationDTO.getPropertyAddress())
+                    .damageType(habilitationDTO.getDamageType())
+                    .build();
+            habilitationRepository.save(sinistre);
+            try {
+                String imageUrl = imageService.uploadImage(image);
+                Image imageSaved = Image.builder()
+                        .imageUrl(imageUrl)
+                        .name("user : " + foundUser.getId() + " Reclamation Image")
+                        .sinistre(sinistre)
+                        .build();
+                imageList.add(imageSaved);
+                imageRepository.save(imageSaved);
+                sinistre.setImages(imageList);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            }
+            try {
+                String documentUrl = documentService.uploadDocument(document);
+                Document documentSaved = Document.builder()
+                        .documentUrl(documentUrl)
+                        .documentName("user : " + foundUser.getId() + " Reclamation Image")
+                        .sinistre(sinistre)
+                        .build();
+                documentList.add(documentSaved);
+                documentRepository.save(documentSaved);
+                sinistre.setDocument(documentList);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            }
+            sinistreRepository.save(sinistre);
+            List<Sinistre> sinistres = foundUser.getSinistres();
+            if (sinistres == null) {
+                sinistres = new ArrayList<>();
+            }
+            sinistres.add(sinistre);
+            foundUser.setSinistres(sinistres);
+            userRepository.save(foundUser);
+
+            /*emailSenderService.sendEmail(foundUser.getEmail(), "Sinistre", "Sinistre with ID :" + sinistre.getId() +
+                    " created at the date :" + LocalDateTime.now() +
+                    " with Sinistre status  :" + sinistre.getEtat());*/
+            return ResponseEntity.ok("User Sinistre saved Successfully");
+        } else {
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).body("user not found");
+        }
+    }
+
+    public ResponseEntity<List<Habilitation>> getHabilitationSinistre() {
+        List<Habilitation> sinistres = habilitationRepository.findAll();
+        return ResponseEntity.ok(sinistres);
+    }
+
+    public ResponseEntity<?> GetHabilitationSinistres(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()){
+            List<Habilitation> habilitationsList = habilitationRepository.findByUserId(userId);
+            return ResponseEntity.ok(habilitationsList);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found");
+        }
+    }
+}
