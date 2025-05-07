166b327fca089fa714b1c85b00429eee51f27b84
Iskander Romdhane <skander20034@gmail.com>
2025-05-07 13:39:56 +0100

    Updated Chatbot widgetDisplay logique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,21 @@ import Login from "./Pages/Auth/Login.jsx";
 import Unauthorized from "./Pages/Unauthorized.jsx";
 import AboutUs from "./Pages/AboutUs.jsx";
 import ContactUs from "./Pages/ContactUs/ContactUs.jsx";
-import Dashboard from "./Pages/User/Dashboard.jsx";
-import DeposerReclamations from "./Pages/User/Reclamations/DeposerReclamations.jsx";
-import SideBar from "./Components/SideBar.jsx";
-import Agences from "./Pages/User/Agences/Agences.jsx";
-import ConsulterReclamation from "./Pages/User/Reclamations/ConsulterReclamation.jsx";
-import AdminDashboard from "./Pages/Admin/AdminDashboard.jsx";
-import AdminSideBar from "./Components/AdminSideBar.jsx";
-import UsersReclamations from "./Pages/Admin/Reclamations/UsersReclamations.jsx";
-import ReclamationDetails from "./Pages/Admin/Reclamations/ReclamationDetails.jsx";
-import Navbar from "./Components/Navbar.jsx";
+import Dashboard from './Pages/User/Dashboard.jsx';
+import DeposerReclamations from './Pages/User/Reclamations/DeposerReclamations.jsx';
+import SideBar from './Components/SideBar.jsx';
+import Agences from './Pages/User/Agences/Agences.jsx';
+import ConsulterReclamation from './Pages/User/Reclamations/ConsulterReclamation.jsx';
+import AdminDashboard from './Pages/Admin/AdminDashboard.jsx';
+import AdminSideBar from './Components/AdminSideBar.jsx';
+import UsersReclamations from './Pages/Admin/Reclamations/UsersReclamations.jsx';
+import ReclamationDetails from './Pages/Admin/Reclamations/ReclamationDetails.jsx';
+import Navbar from './Components/Navbar.jsx';
 import PasswordPage from "./Pages/Auth/PasswordPage.jsx";
-import ChatbotWidget from "./Components/ChatbotWidget.jsx";
-import ConsulterSinistre from "./Pages/User/Sinistres/ConsulterSinistre.jsx";
-import CreerSinistres from "./Pages/User/Sinistres/CreerSinistres.jsx";
-import SinistrePage from "./Pages/User/Sinistres/SinistrePage.jsx";
+import ChatbotWidget from './Components/ChatbotWidget.jsx';
+import ConsulterSinistre from './Pages/User/Sinistres/ConsulterSinistre.jsx';
+import CreerSinistres from './Pages/User/Sinistres/CreerSinistres.jsx';
+import SinistrePage from './Pages/User/Sinistres/SinistrePage.jsx';
 import NotificationsPage from "./Pages/NotificationsPage.jsx";
 import NotificationDetail from "./Pages/NotificationDetail.jsx";
 import UsersTable from "./Pages/Admin/user managment/UsersTable.jsx";
@@ -31,18 +31,24 @@ import { FAQPage } from "./Components/FAQs/FAQPage.js";
 import SinistresTable from "./Pages/Admin/Sinistres/SinistresTable.jsx";
 import SinistresDetails from "./Pages/Admin/Sinistres/SinistresDetails.jsx";
 import Profil from "./Pages/Profil/Profil.jsx";
-import { MyDashboardLayout } from "./Components/dashboard/DashboardLayout.tsx";
-import { AdminDashboardLayout } from "./Components/adminDashboard/layout/MyDashboardLayout.tsx";
-import { MyDashboard } from "./Components/adminDashboard/dashboard/Dashboard.tsx";
-
+import {MyDashboardLayout} from "./Components/dashboard/DashboardLayout.tsx"
 const DashboardLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  const role = localStorage.getItem("client_role");
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem("client_role");
+    setRole(storedRole);
+  }, []);
+
+  if (!role) return null; 
+
   const SidebarComponent = role === "client_admin" ? AdminSideBar : SideBar;
 
   return (
     <div className="flex">
       <SidebarComponent isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+      {role === "client_user" && <ChatbotWidget />}
       <div
         className={`flex flex-col flex-1 transition-all duration-300 ${
           sidebarOpen ? "ml-64" : "ml-16"
@@ -58,7 +64,7 @@ const DashboardLayout = ({ children }) => {
 function App() {
   return (
     <Router>
-      <ChatbotWidget />
+      
       <Routes>
         {/* Public routes */}
         <Route path="/" element={<Root />} />
@@ -73,14 +79,6 @@ function App() {
           element={
             <DashboardLayout>
               <MyDashboardLayout />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/AdminDashboard"
-          element={
-            <DashboardLayout>
-              <MyDashboard />
             </DashboardLayout>
           }
         />
