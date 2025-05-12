4e5d7adcd95d5f0e538a41f2d403edebe6e60727
Iskander Romdhane <skander20034@gmail.com>
2025-05-12 17:24:54 +0100

    Colors file

---
+++ frontend/src/Pages/User/Sinistres/statutCouleurs.js
@@ -0,0 +1,24 @@
+ export const statutCouleurs = {
+    ACCEPTED: {
+      bg: "bg-green-500",
+      text: "text-white",
+      border: "border-green-700",
+    },
+    PENDING: {
+      bg: "bg-yellow-500",
+      text: "text-white",
+      border: "border-yellow-500",
+    },
+    REJECTED: {
+      bg: "bg-red-500",
+      text: "text-white",
+      border: "border-red-500",
+    },
+    UNDER_REVIEW: {
+      bg: "bg-blue-500",
+      text: "text-white",
+      border: "border-blue-500",
+    },
+  };
+
+export default statutCouleurs ;
