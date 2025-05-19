bce5df2bbb0695f335e4e049d50816eb5a272722
Iskander Romdhane <skander20034@gmail.com>
2025-05-19 17:01:41 +0100

    Fixed Null Image urls error

--- backend/src/main/java/com/backend/Insurance/User/Mapper/UserMapper.java
+++ backend/src/main/java/com/backend/Insurance/User/Mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.backend.Insurance.User.Mapper;
 
+import com.backend.Insurance.Image.Entity.Image;
 import com.backend.Insurance.User.DTO.UserDTO;
 import com.backend.Insurance.User.User;
 import org.mapstruct.Mapper;
@@ -10,9 +11,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "profilePictureUrl" , source = "profilePicture.imageUrl" )
-    @Mapping(target = "active" ,source = "active")
+    @Mapping(target = "profilePictureUrl", source = "profilePicture.imageUrl")
+    @Mapping(target = "active", source = "active")
     UserDTO toDto(User user);
 
-    List<UserDTO> toDtoList(List<User> user);
+    List<UserDTO> toDtoList(List<User> users);
+    default String mapProfilePicture(Image image) {
+        return image != null ? image.getImageUrl() : null;
+    }
 }
